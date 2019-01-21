f4d19972481242174f267e18f82030ab21f53e0c
Unknown <regpo55555@gmail.com>
2019-01-21 15:11:59 +0600

    Fixed link to flibusta.net

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+  htmlparser2
+} from "react-html-parser";
 
 // import SearchInput from './components/SearchInput'
-import './App.css';
-import SearchResult from './components/SearchResult';
-
+import "./App.css";
+import SearchResult from "./components/SearchResult";
 
 // var axiosTest = axios.get('https://api.github.com/repos/regfe89/keyboardnoble').data.forks_url;
 // const axiosTest = async () => {
@@ -21,16 +24,13 @@ import SearchResult from './components/SearchResult';
 //   return await Promise axiosTest()
 // }
 
-
-
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      querie: '',
-      search: '',
-      result: '',
+      querie: "",
+      search: "",
+      result: "",
       result2: [],
       buttons: [],
       seriesChecked: true,
@@ -52,27 +52,31 @@ class App extends Component {
 
   handleChecked(event) {
     switch (event.target.name) {
-      case 'series':
+      case "series":
         this.setState({ seriesChecked: !this.state.seriesChecked });
         break;
-      case 'author':
+      case "author":
         this.setState({ authorChecked: !this.state.authorChecked });
         break;
-      case 'book':
+      case "book":
         this.setState({ bookChecked: !this.state.bookChecked });
         break;
-      case 'genre':
+      case "genre":
         this.setState({ genreChecked: !this.state.genreChecked });
         break;
     }
   }
 
   changePageNumber(arg) {
-    if(arg === 1) {
-      this.setState({ pageNumber: this.state.pageNumber - 1 })
+    if (arg === 1) {
+      this.setState({ pageNumber: this.state.pageNumber - 1 }, () =>
+        this.handleSearch()
+      );
     }
-    if(arg === 2) {
-      this.setState({ pageNumber: this.state.pageNumber + 1 })
+    if (arg === 2) {
+      this.setState({ pageNumber: this.state.pageNumber + 1 }, () =>
+        this.handleSearch()
+      );
     }
   }
 
@@ -81,33 +85,49 @@ class App extends Component {
     // &chs=on
     let preQuerie = this.state.querie.replace(/ /g, "+");
     if (this.state.seriesChecked) {
-      preQuerie = preQuerie + '&chs=on'
+      preQuerie = preQuerie + "&chs=on";
     }
     if (this.state.authorChecked) {
-      preQuerie = preQuerie + '&cha=on'
+      preQuerie = preQuerie + "&cha=on";
     }
     if (this.state.bookChecked) {
-      preQuerie = preQuerie + '&chb=on'
+      preQuerie = preQuerie + "&chb=on";
     }
     // if (this.state.genreChecked) {
     //   preQuerie = preQuerie + '&chg=on'
     // }
-    const searchQuerie = 'https://flibustasearch.herokuapp.com/http://flibusta.is/booksearch?page='+this.state.pageNumber+'&ask=' + preQuerie;
-    axios.get(searchQuerie).then(response => { this.setState({ result: response.data }, () => this.refineResult()) })
+    const searchQuerie =
+      "https://flibustasearch.herokuapp.com/http://flibusta.is/booksearch?page=" +
+      this.state.pageNumber +
+      "&ask=" +
+      preQuerie;
+    axios.get(searchQuerie).then(response => {
+      this.setState({ result: response.data }, () => this.refineResult());
+    });
   }
 
   refineResult() {
     const result0 = String(this.state.result);
-    const result1 = result0.substring(this.state.result.indexOf('<h1 class="title">Поиск книг</h1>') + 0);
-    const result2 = result1.substring(0, result1.indexOf('<div id="sidebar-right" class="sidebar">'));
-    const array1 = result2.split('\n');
+    const result1 = result0.substring(
+      this.state.result.indexOf('<h1 class="title">Поиск книг</h1>') + 0
+    );
+    const result2 = result1.substring(
+      0,
+      result1.indexOf('<div id="sidebar-right" class="sidebar">')
+    );
+    const array1 = result2.split("\n");
     const array2 = array1.filter(String);
-    const pagesTotal = array2.filter(elem => {
-      if (elem.includes('h1 class="title"')) { 
+    let pagesTotal = array2.filter(elem => {
+      if (
+        elem.includes('class="pager"') ||
+        elem.includes('<li class="pager-item"')
+      ) {
         return true;
-      }; 
+      }
     });
-    
+    console.log(pagesTotal.length);
+    console.log(pagesTotal);
+
     // const preButtonArray = array2.map(elem => {
     //   if (elem.includes('class="pager') && !elem.includes('<div class="item-list"><ul class="pager">')) {
     //     return elem
@@ -132,71 +152,146 @@ class App extends Component {
     // })
 
     const array3 = array2.filter(elem => {
-      if (elem.includes('h1 class="title"')) { return false };
-      if (elem.includes('input type=submit value')) { return false };
-      if (elem.includes('<input type="checkbox"')) { return false };
-      if (elem.includes('<a href="http://fbsearch.ru">')) { return false };
-      if (elem.includes('class="pager')) { return false };
+      if (elem.includes('h1 class="title"')) {
+        return false;
+      }
+      if (elem.includes("input type=submit value")) {
+        return false;
+      }
+      if (elem.includes('<input type="checkbox"')) {
+        return false;
+      }
+      if (elem.includes('<a href="http://fbsearch.ru">')) {
+        return false;
+      }
+      if (elem.includes('class="pager')) {
+        return false;
+      }
       // if (elem.includes('</form><hr><br><div class="item-list"><ul class="pager">')) { return false };
       return true;
     });
     // const array6 = array3.map
 
     const array5 = array3.map((elem, index) => {
-
-      if (elem.includes('<ul>')) {
-        elem = elem.substr(elem.indexOf('<ul>') + 4)
+      if (elem.includes("<ul>")) {
+        elem = elem.substr(elem.indexOf("<ul>") + 4);
         // console.log('found!')
-      };
+      }
+      elem = elem.replace(
+        '<span style="background-color: #FFFCBB"></span>',
+        ""
+      );
+      elem = elem.replace("<b>", "");
+      elem = elem.replace("</b>", "");
+      elem = elem.replace(/<a href="\//g, "<a href=\"http://flibusta.is/");
 
       // if (elem.includes('<li class="pager-')) {
       //   elem = ReactHtmlParser(elem)
       // };
-      elem = ReactHtmlParser(elem)
-      return (
-        elem
-      )
-    })
+      return elem;
+    });
+
+    const array6 = array5.map((elem, index) => {
+      elem = ReactHtmlParser(elem);
+      return elem;
+    });
 
     // let doc = new DOMParser().parseFromString('<div><b>Hello!</b></div>', 'text/html');
-    this.setState({ result2: array5, pagesTotal: pagesTotal.length });
+    this.setState({ result2: array6, pagesTotal: pagesTotal.length / 2 });
   }
 
   render() {
+    let prevButton = "Hidden";
+    if (this.state.pageNumber > 0) {
+      prevButton = "Button";
+    }
+
+    let nextButton = "Button";
+    if (this.state.pageNumber >= this.state.pagesTotal - 1) {
+      nextButton = "Hidden";
+    }
+
+    let isPages = "Hidden";
+    if (this.state.pagesTotal !== 0) {
+      isPages = "Button";
+    }
 
     return (
-      <div className='App'>
-        <input placeholder='Enter book name' type="text" value={this.state.value} onChange={this.handleChange} />
+      <div className="App">
+        <input
+          placeholder="Enter book name"
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
 
         {/* <SearchInput
           placeholder='Enter book name' /> */}
         <button onClick={this.handleSearch}>Search</button>
         <div>
-          <input type="checkbox" name="series" value="newsletter" onChange={this.handleChecked} checked={this.state.seriesChecked} /><label>Серии</label>
+          <input
+            type="checkbox"
+            name="series"
+            value="newsletter"
+            onChange={this.handleChecked}
+            checked={this.state.seriesChecked}
+          />
+          <label>Серии</label>
         </div>
         <div>
-          <input type="checkbox" name="author" value="newsletter" onChange={this.handleChecked} checked={this.state.authorChecked} /><label>Авторы</label>
+          <input
+            type="checkbox"
+            name="author"
+            value="newsletter"
+            onChange={this.handleChecked}
+            checked={this.state.authorChecked}
+          />
+          <label>Авторы</label>
         </div>
         <div>
-          <input type="checkbox" name="book" value="newsletter" onChange={this.handleChecked} checked={this.state.bookChecked} /><label>Книги</label>
+          <input
+            type="checkbox"
+            name="book"
+            value="newsletter"
+            onChange={this.handleChecked}
+            checked={this.state.bookChecked}
+          />
+          <label>Книги</label>
         </div>
         <div>
-          <input type="checkbox" name="genre" value="newsletter" onChange={this.handleChecked} checked={this.state.genreChecked} /><label>Жанры</label>
+          <input
+            type="checkbox"
+            name="genre"
+            value="newsletter"
+            onChange={this.handleChecked}
+            checked={this.state.genreChecked}
+          />
+          <label>Жанры</label>
         </div>
-        <div className='Pages'>
-          <button className='Button' onClick={() => this.changePageNumber(1)}>Предыдущая</button>
-          <p className='Button'>Страница {this.state.pageNumber+1}</p>
-          <button className='Button' onClick={() => this.changePageNumber(2)}>Следующая</button>
+        <div className="Pages">
+          <button
+            className={prevButton}
+            onClick={() => this.changePageNumber(1)}
+          >
+            Предыдущая
+          </button>
+          <p className={isPages}>
+            Страница {this.state.pageNumber + 1} из {this.state.pagesTotal}
+          </p>
+          <button
+            className={nextButton}
+            onClick={() => this.changePageNumber(2)}
+          >
+            Следующая
+          </button>
         </div>
         <div>
           {this.state.result2.map((result, index) => {
             return (
               <div key={index}>
-                <SearchResult
-                  result={result}
-                />
+                <SearchResult result={result} />
               </div>
-            )
+            );
           })}
         </div>
       </div>
